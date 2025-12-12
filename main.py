7619fabcb507734856370ffefc144182e4f000dc
Kevin Meng <85904902+kmeng9025@users.noreply.github.com>
2025-12-11 20:38:36 -0500

    push

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ time.sleep(2)
 while (True):
     frame = cam.capture_array()
     frame = frame[364:545, 44 : 1120]
-    cv2.imshow("Display", frame)
+    pre_bil = cv2.bilateralFilter(frame, 5, 80, 80)
+    hsv = cv2.cvtColor(pre_bil, cv2.COLOR_BGR2HSV)
+    thresh = cv2.adaptiveThreshold(cv2.cvtColor(pre_bil, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                    cv2.THRESH_BINARY, 59, 5)
+    thresh = cv2.bitwise_not(thresh)
+    cv2.imshow("Display", thresh)
     time.sleep(2)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
