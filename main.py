5b92421c5f3c92b8d7163866e46c2399bac45a48
Kevin Meng <85904902+kmeng9025@users.noreply.github.com>
2025-12-11 19:18:53 -0500

    push

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import cv2
 from picamera2 import Picamera2
 import time
 
-picam2 = Picamera2()
-config = picam2.create_still_configuration(main={"size": (1920, 1080)})
-picam2.configure(config)
-picam2.start()
+cam = Picamera2()
+cam.preview_configuration.main.size = (1280, 720)
+cam.preview_configuration.main.format = "RGB888"
+cam.configure("preview")
+cam.start()
 time.sleep(2)
 while (True):
-    frame = picam2.capture_array()
+    frame = cam.capture_array()
     # frame = frame[1920*0.2 : 1920*0.8 , 1080*0.2:1080*0.2]
     cv2.imshow("Display", frame)
     time.sleep(2)
