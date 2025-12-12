f86e476fffc789784b18926a8280a1c976d969e9
Kevin Meng <85904902+kmeng9025@users.noreply.github.com>
2025-12-11 20:30:17 -0500

    push

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ cam.start()
 time.sleep(2)
 while (True):
     frame = cam.capture_array()
-    frame = frame[44 : 1120, 364:545]
+    frame = frame[364:545, 44 : 1120]
     cv2.imshow("Display", frame)
     time.sleep(2)
     key = cv2.waitKey(1) & 0xFF
