a684062b22a0a40800ccdf5b3f9603a2e41b5ddc
Kevin Meng <85904902+kmeng9025@users.noreply.github.com>
2025-12-24 20:39:05 -0500

    push

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while (True):
     frame = cam.capture_array()
     frame = frame[364:545, 44 : 1120]
     cv2.imshow("Display", frame)
-    time.sleep(2)
+    # time.sleep(2)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
@@ -29,7 +29,7 @@ while (True):
         "AeEnable": False,
         "AwbEnable": False,
     })
-    time.sleep(1.1)
+    time.sleep(1)
     print("currentExposureTime", exposureTime)
 cam.stop()
 cv2.destroyAllWindows()
