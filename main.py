afd691d01f8299d362de2efd4630447d7ac7bb8b
kmeng9025 <kmeng90251@gmail.com>
2026-01-01 20:24:04 -0500

    push

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ time.sleep(1)
 
 while (True):
     frame = cam.capture_array()
-    frame = frame[364:545, 44 : 1120]
+    frame = frame[364:540, 44:1120]
     cv2.imshow("Display", frame)
     # time.sleep(2)
     key = cv2.waitKey(1) & 0xFF
